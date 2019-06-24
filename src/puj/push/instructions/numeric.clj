1c5b8d7aae9285b345ce6fcea5a6d16b57d56e3e
erp12 <eddie.pantridge@gmail.com>
2019-06-24 18:35:37 -0400

    Added some comments to int-add for clarity

--- src/puj/push/instructions/numeric.clj
+++ src/puj/push/instructions/numeric.clj
@@ -7,8 +7,10 @@
    :int-add
    (with-meta
      (u/->SimpleInstruction
-       [:int :int] [:int] 0
-       #(vector (+ %1 %2)))
+       [:int :int]          ; Takes two arguments from the :int stack
+       [:int]               ; Pushes a single result to the :int stack.
+       0                    ; The instruction opens 0 code blocks.
+       #(vector (+ %1 %2))) ; The function which turns arguments to results.
      {:puj.push.instruction/doc  "Pushes the sum of the top 2 ints onto the int stack."})
 
    :int-sub
