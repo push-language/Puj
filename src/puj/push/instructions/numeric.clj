1e84cc034ce50cb5e394d0fb2ea2cda1fa6c6cfe
Tom Helmuth <thelmuth@hamilton.edu>
2019-06-26 12:49:22 -0400

    Implemented int-inc instruction.

--- src/puj/push/instructions/numeric.clj
+++ src/puj/push/instructions/numeric.clj
@@ -16,13 +16,20 @@
      (u/->SimpleInstruction
        [:int :int] [:int] 0
        #(vector (- %1 %2)))
-     {:puj.push.instruction/doc  "Pushes the difference of the top 2 into onto the int stack."})
+     {:puj.push.instruction/doc  "Pushes the difference of the top 2 ints onto the int stack."})
 
    :int-mult
    (with-meta
      (u/->SimpleInstruction
        [:int :int] [:int] 0
        #(vector (* %1 %2)))
-     {:puj.push.instruction/doc  "Pushes the product of the top 2 into onto the int stack."})
+     {:puj.push.instruction/doc  "Pushes the product of the top 2 ints onto the int stack."})
+
+   :int-inc
+   (with-meta
+     (u/->SimpleInstruction
+      [:int] [:int] 0
+      #(vector (inc %)))
+     {:puj.push.instruction/doc "Pushes the increment of the top int onto the int stack."})
 
    })
