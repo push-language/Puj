daaa4adf5f6cd9535785bcc2ce970933ecd1cca0
erp12 <eddie.pantridge@gmail.com>
2020-06-24 01:05:09 -0400

    Adds namespace to test core instructions

--- src/puj/push/instructions/text.clj
+++ src/puj/push/instructions/text.clj
@@ -7,12 +7,12 @@
    (with-meta
      (i/simple-instruction
        :string-take [:string :int] :string 0
-       #(apply str (take %2 %1)))
+       #(apply str (take %1 %2)))
      {:puj.push.instruction/doc "Pushes the first `n` characters of the top string as a new string."})
 
    (with-meta
      (i/simple-instruction
        :string-drop [:string :int] :string 0
-       #(apply str (drop %2 %1)))
+       #(apply str (drop %1 %2)))
      {:puj.push.instruction/doc "Pushes the first `n` characters of the top string as a new string."})
   ])
