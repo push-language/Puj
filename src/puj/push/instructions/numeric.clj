d01a8865898b6cbad236efeaaff71d46efbb8d37
Tom Helmuth <thelmuth@hamilton.edu>
2020-07-03 16:20:44 -0400

    Added :int-mod and :int-dec instructions, and a few more tests.

--- src/puj/push/instructions/numeric.clj
+++ src/puj/push/instructions/numeric.clj
@@ -21,8 +21,18 @@
      (i/simple-instruction :int-mult [:int :int] :int 0 *)
      {:puj.push.instruction/doc  "Pushes the product of the top 2 ints onto the int stack."})
 
+   ; @TODO :int-div - what kind of protected division should we use? push-like, which would no-op if divide by zero, or analytic quotient, as described here: https://push-language.hampshire.edu/t/gptp-2019-notes/
+
+   (with-meta
+     (i/simple-instruction :int-mod [:int :int] :int 0 mod)
+     {:puj.push.instruction/doc "Pushes the mod of the top 2 ints onto the int stack."})
+
    (with-meta
      (i/simple-instruction :int-inc [:int] :int 0 inc)
      {:puj.push.instruction/doc "Pushes the increment of the top int onto the int stack."})
+
+   (with-meta
+     (i/simple-instruction :int-dec [:int] :int 0 dec)
+     {:puj.push.instruction/doc "Pushes the decrement of the top int onto the int stack."})
 
    ])
